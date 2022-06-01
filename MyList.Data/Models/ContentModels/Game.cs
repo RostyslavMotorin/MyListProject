cf9106714cb051800cc02f045ac6349fe6917eeb
Nemelas <49837703+Nemelaskin@users.noreply.github.com>
2022-06-01 22:56:37 +0300

    add game model

---
+++ MyList.Data/Models/ContentModels/Game.cs
@@ -0,0 +1,19 @@
+﻿using MyList.Data.Models.Tags;
+
+namespace MyList.Data.Models.ContentModels
+{
+    public class Game
+    {
+        public Guid GameID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureURL { get; set; }
+        public ICollection<GameTag> Tags { get; set; } = new List<GameTag>();
+        public int UserScore { get; set; } //enum
+        public int GlobalScore { get; set; } //enum
+        public string UserStatus { get; set; } //enum
+        public string GlobalStatus { get; set; } //enum
+        public string GameStudio { get; set; }
+        public DateTime RelizeDate { get; set; }
+    }
+}
---
+++ MyList.Data/Models/Tags/GameTag.cs
@@ -0,0 +1,8 @@
+﻿namespace MyList.Data.Models.Tags
+{
+    public class GameTag
+    {
+        public Guid TagID { get; set; }
+        public string Name { get; set; }
+    }
+}
