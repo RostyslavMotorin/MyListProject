748eba802296969fa5a92b229679d114a20e1c30
Nemelas <49837703+Nemelaskin@users.noreply.github.com>
2022-06-01 22:51:36 +0300

    add film model

--- MyList.Data/Models/ContentModels/Film.cs
+++ MyList.Data/Models/ContentModels/Film.cs
@@ -2,16 +2,15 @@
 
 namespace MyList.Data.Models.ContentModels
 {
-    public class Anime
+    public class Film
     {
-        public Guid AnimeID { get; set; }
+        public Guid FilmID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string PictureURL { get; set; }
-        public ICollection<AnimeTag> Tags { get; set; } = new List<AnimeTag>();
+        public ICollection<FilmTag> Tags { get; set; } = new List<FilmTag>();
         public int UserScore { get; set; } //enum
         public int GlobalScore { get; set; } //enum
-        public int CountEpisodes { get; set; }
         public string UserStatus { get; set; } //enum
         public string GlobalStatus { get; set; } //enum
         public ICollection<Creators> Authors { get; set; } = new List<Creators>();
---
+++ MyList.Data/Models/Tags/FilmTag.cs
@@ -0,0 +1,8 @@
+﻿namespace MyList.Data.Models.Tags
+{
+    public class FilmTag
+    {
+        public Guid TagID { get; set; }
+        public string Name { get; set; }
+    }
+}
